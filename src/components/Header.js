37a09a959e1efdd3d1d5d705f245e5f1b0e707bf
yeffry <yeffry45@gmail.com>
2022-06-15 13:38:49 -0400

    add sidebar Header jsx component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,54 +1,59 @@
 import { AiFillHome } from "react-icons/ai";
 import { FaGamepad } from "react-icons/fa";
+import { FiLogOut } from "react-icons/fi";
 
-import styles from './Header.module.css'
+import styles from "./Header.module.css";
 
 const Header = () => {
   return (
     <header className={styles.header}>
       <nav className={styles["header-nav"]}>
         <div className={styles["header-icon"]}>
-          <FaGamepad size={70} color="white" />
+          <FaGamepad size={70} color="#EFF2F4" />
         </div>
-        <div children="header-links">
-          <div>
+        <div children={styles["header-links"]}>
+          <div className={styles.links}>
             <a href="">
-              <AiFillHome />
-              Home
+              <AiFillHome size={25} />
+              <span>Home</span>
             </a>
           </div>
 
-          <div>
-            <a href="">
-              <AiFillHome />
-              Library
+          <div className={styles.links}>
+            <a href="" className={styles.active}>
+              <AiFillHome size={25} />
+              <span>Library</span>
             </a>
           </div>
 
-          <div>
+          <div className={styles.links}>
             <a href="">
-              <AiFillHome />
-              About
+              <AiFillHome size={25} />
+              <span>About</span>
             </a>
           </div>
 
-          <div>
+          <div className={styles.links}>
             <a href="">
-              <AiFillHome />
-              Placeholder
+              <AiFillHome size={25} />
+              <span>Placeholder</span>
             </a>
           </div>
 
-          <div>
+          <div className={styles.links}>
             <a href="">
-              <AiFillHome />
-              Placeholder
+              <AiFillHome size={25} />
+              <span>Placeholder</span>
+            </a>
+          </div>
+
+          <div className={`${styles.links}`}>
+            <a href="">
+              <FiLogOut size={25} />
+              <span>Logout</span>
             </a>
           </div>
         </div>
-
-        <div>Light/Dark Theme</div>
-        <div>Logout div</div>
       </nav>
     </header>
   );
