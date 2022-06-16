b78790c74dd2514d2f238f92aa233c4245b02d1b
yeffry <yeffry45@gmail.com>
2022-06-16 13:56:49 -0400

    add react router with dummy components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { Routes, Route} from "react-router-dom";
+
 import Header from "./components/Header";
+import Home from "./components/Home";
+import About from "./components/About";
 
 function App() {
   return (
     <React.Fragment>
-      <Header/>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+      </Routes>
     </React.Fragment>
   );
 }
