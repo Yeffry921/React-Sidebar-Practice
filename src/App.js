c2ce23c0b02c7284343f06469865a85a0d30c21e
yeffry <yeffry45@gmail.com>
2022-06-21 15:27:32 -0400

    add showcase data fetch on load

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 
 import styles from "./layout.module.css";
@@ -9,6 +9,20 @@ import About from "./components/About";
 import MainSearch from "./components/MainSearch";
 
 function App() {
+  const [showcase, setShowcase] = useState([]);
+
+  useEffect(() => {
+    fetchShowcase();
+  }, []);
+
+  const fetchShowcase = async () => {
+    const data = await fetch(
+      "https://api.rawg.io/api/games?key=6f59240c598f49feaf7fd46a9990b4ff&metacritic=85,100&dates=2015-01-01,2022-03-31"
+    );
+    const response = await data.json();
+    setShowcase(response.results);
+  };
+
   return (
     <React.Fragment>
       <div className={styles.container}>
@@ -16,12 +30,10 @@ function App() {
 
         <main className={styles["main-container"]}>
           <MainSearch />
-          <div className={styles["body-output"]}>
-            <Routes>
-              <Route path="/" element={<Home />}></Route>
-              <Route path="/about" element={<About />}></Route>
-            </Routes>
-          </div>
+          <Routes>
+            <Route path="/" element={<Home showcase={showcase}/>}></Route>
+            <Route path="/about" element={<About />}></Route>
+          </Routes>
         </main>
       </div>
     </React.Fragment>
