b7e784e8a809b791ba46bd6092f77244e16caf73
yeffry <yeffry45@gmail.com>
2022-06-20 22:34:12 -0400

    add layout classes, re-structured the main app component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React from "react";
-import { Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
-import Header from "./components/Header";
+import styles from './layout.module.css'
+
+import Header from "./components/Side";
 import Home from "./components/Home";
 import About from "./components/About";
 
 function App() {
   return (
     <React.Fragment>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/about" element={<About />}></Route>
-      </Routes>
+      <div className={styles.container}>
+        <div className={styles.sidebar}>
+          <Header />
+        </div>
+
+        <div className={styles["body-container"]}>
+          <div className="body-search">
+            <input type="text" placeholder="Placeholder input"/>
+          </div>
+          <div className={styles["body-output"]}>
+            <Routes>
+              <Route path="/" element={<Home />}></Route>
+              <Route path="/about" element={<About />}></Route>
+            </Routes>
+          </div>
+        </div>
+      </div>
     </React.Fragment>
   );
 }
