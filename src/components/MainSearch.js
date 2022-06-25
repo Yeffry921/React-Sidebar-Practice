c29ca96a38159a4a6b0aadfd6192036944218599
yeffry <yeffry45@gmail.com>
2022-06-25 12:30:07 -0400

    add functionality for searching games

--- src/components/MainSearch.js
+++ src/components/MainSearch.js
@@ -1,21 +1,36 @@
 import React, { useState } from "react";
+import { useEffect } from "react";
 
 import placeholder from "../assets/profile-placeholder.png";
 import styles from "./MainSearch.module.css";
 import SearchBar from "./SearchBar";
 
-const data = [
-  { title: "New game" },
-  { title: "New game2" },
-  { title: "Hello" },
-];
 
 const MainSearch = () => {
+  const [searchTerm, setSearchTerm] = useState('')
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    const searchGame = async () => {
+      const url = `https://api.rawg.io/api/games?key=6f59240c598f49feaf7fd46a9990b4ff&search=${searchTerm}`
+      const data = await fetch(url)
+      const response = await data.json()
+      setData(response.results)
+    }
+
+    searchGame()
+    
+  }, [searchTerm])
+
+  const getSearchTerm = (term) => {
+    setSearchTerm(term)
+  }
+
   return (
     <React.Fragment>
       <div className={styles["main-search"]}>
         <div className={styles.wrapper}>
-          <SearchBar placeholder="Search games..." data={data} />
+          <SearchBar placeholder="Search games..." data={data} onGetSearch={getSearchTerm} />
           <img src={placeholder} alt="" className="" />
         </div>
       </div>
