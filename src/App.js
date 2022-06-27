a42d56f78acfee99def807c7c1b02019a0d23d1d
yeffry <yeffry45@gmail.com>
2022-06-27 11:56:39 -0400

    refactor data fetching into Home component

--- src/App.js
+++ src/App.js
@@ -1,37 +1,24 @@
-import React, {useState, useEffect } from "react";
+import React from "react";
 import { Routes, Route } from "react-router-dom";
 
 import styles from "./layout.module.css";
 
 import Sidebar from "./components/Sidebar";
-import Showcase from "./components/Showcase";
-import About from "./components/About";
+import About from "./components/pages/About";
 import MainSearch from "./components/MainSearch";
+import Home from "./components/pages/Home";
 
 function App() {
-  const [showcase, setShowcase] = useState([]);
-
-  useEffect(() => {
-    fetchShowcase();
-  }, []);
-
-  const fetchShowcase = async () => {
-    const data = await fetch(
-      "https://api.rawg.io/api/games?key=6f59240c598f49feaf7fd46a9990b4ff&metacritic=85,100&dates=2015-01-01,2022-03-31"
-    );
-    const response = await data.json();
-    setShowcase(response.results);
-  };
 
   return (
     <React.Fragment>
       <div className={styles.container}>
         <Sidebar />
-
         <main className={styles["main-container"]}>
           <MainSearch />
           <Routes>
-            <Route path="/" element={<Showcase title="Popular Releases" showcase={showcase}/>}></Route>
+            <Route path="/" element={<Home /> }></Route>
+            <Route path="/new" element={<About />}></Route>
             <Route path="/about" element={<About />}></Route>
           </Routes>
         </main>
