68e453ecf1d7900ea4c67396c89ae2f09dd53184
yeffry <yeffry45@gmail.com>
2022-06-29 12:26:04 -0400

    move date logic outside component

--- src/components/NewReleases.js
+++ src/components/NewReleases.js
@@ -5,7 +5,9 @@ import Showcase from "./Showcase";
 const date_ob = new Date()
 const currentDate = date_ob.toISOString().slice(0, 10)
 
-
+let day = ("0" + date_ob.getDate()).slice(-2);
+let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
+let year = date_ob.getFullYear() - 1;
 
 const NewReleases = () => {
   const [showcase, setShowcase] = useState([]);
@@ -14,11 +16,7 @@ const NewReleases = () => {
     fetchShowcase();
   }, []);
 
-  let day = ("0" + date_ob.getDate()).slice(-2);
-  let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
-  let year = date_ob.getFullYear() - 1;
   const fetchShowcase = async () => {
-  
     const data = await fetch(
       `https://api.rawg.io/api/games?key=6f59240c598f49feaf7fd46a9990b4ff&dates=${year}-${month}-${day},${currentDate}`
     );
