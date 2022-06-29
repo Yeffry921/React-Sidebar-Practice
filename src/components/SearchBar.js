50234dcb380b2f9efb481c0ff03335ff37236688
yeffry <yeffry45@gmail.com>
2022-06-29 12:28:02 -0400

    replace anchor tags with Link tags + add callback for getting gameId

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,11 @@
 import { useState } from "react";
+import { Link } from 'react-router-dom'
 import SearchIcon from "@mui/icons-material/Search";
 import CloseIcon from '@mui/icons-material/Close';
 
 import "./SearchBar.css";
 
-const SearchBar = ({ placeholder, data, onGetSearch }) => {
+const SearchBar = ({ placeholder, data, onGetSearch, onHandleId }) => {
   const [filteredData, setFilteredData] = useState([]);
   const [wordEntered, setWordEntered] = useState('')
   
@@ -41,10 +42,10 @@ const SearchBar = ({ placeholder, data, onGetSearch }) => {
         <div className="dataResult">
           {filteredData.slice(0, 15).map((value, key) => {
             return (
-              <a className="dataItem" target="#" key={key}>
+              <Link className="dataItem" to="/game" key={key} onClick={() => onHandleId(value.id)}>
                 <img src={value.background_image} alt="game image" className='dataItem-image'/>
                 <p>{value.name}</p>
-              </a>
+              </Link>
             )
           })}
         </div>
