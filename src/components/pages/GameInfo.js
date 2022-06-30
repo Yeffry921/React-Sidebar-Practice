9e5b5503d2b1027f4480a01930c522570bc50217
yeffry <yeffry45@gmail.com>
2022-06-30 14:27:20 -0400

    add more game stats to GameInfo component

--- src/components/pages/GameInfo.js
+++ src/components/pages/GameInfo.js
@@ -18,39 +18,59 @@ const GameInfo = ({ gameId }) => {
     searchGame();
   }, [gameId]);
 
+  console.log(game);
   const handleText = () => {
-    setDisplayWhole((prevState) => !prevState)
+    setDisplayWhole((prevState) => !prevState);
   };
- 
+
   return (
     <Wrapper className={styles.gameLayout}>
-      <section>
-        {game === null || game.name === "UNDEFINED" ? (
-          <h1>Search for game</h1>
-        ) : (
-          <div>
-            <img src={game.background_image} alt="game image" />
-            <h1>{game.name}</h1>
+      {game === null || game.name === "UNDEFINED" ? (
+        <h1>Search for game</h1>
+      ) : (
+        <div className={styles.content}>
+          <img src={game.background_image} alt="game image" />
+          <h1>{game.name}</h1>
 
-            {/* <ul>{platforms}</ul> */}
-            <ul className={styles.platforms}>
-              {game.platforms.map(({ platform }) => {
-                return <li key={platform.id}>{platform.name}</li>;
-              })}
-            </ul>
-
-            <div className={styles.description}>
-              <h2>Description</h2>
-              <p
-                className={displayWhole === false ? styles.hide : styles.show}
-                onClick={handleText}
-              >
-                {game.description_raw}
-              </p>
+          <ul className={styles.platforms}>
+            {game.platforms.map(({ platform }) => {
+              return <li key={platform.id}>{platform.name}</li>;
+            })}
+          </ul>
+          
+          <div className={styles.flexWrapper}>
+            <div className={styles.column}>
+              <p className={styles.primary}>ESRB</p>
+              <p className={styles.secondary}>{game.esrb_rating.name}</p>
             </div>
+
+            <div className={styles.column}>
+              <p className={styles.primary}>Metacritic</p>
+              <p className={styles.secondary}>{game.metacritic}</p>
+            </div>
+
+            <div className={styles.column}>
+              <p className={styles.primary}>Publisher</p>
+              <p className={styles.secondary}>{game.publishers[0].name}</p>
+            </div>
+
+            <div className={styles.column}>
+              <p className={styles.primary}>Genre</p>
+              <p className={styles.secondary}>{game.genres[0].name}</p>
+            </div>
+            </div>
+            
+          <div className={styles.description}>
+            <h2>Description</h2>
+            <p
+              className={displayWhole === false ? styles.hide : styles.show}
+              onClick={handleText}
+            >
+              {game.description_raw}
+            </p>
           </div>
-        )}
-      </section>
+        </div>
+      )}
     </Wrapper>
   );
 };
